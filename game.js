01449dbf97cd262b7d77d77434f16efb96667c49
Emiliano <emiliano.cabello7@iems.edu.mx>
2023-10-26 22:24:11 -0600

    feat: :art: write a new function why is used by played the first game rock-paper-scissors
    The function choose between rock-paper-scissors and returned the true string whitch is detected by the conditions. The arguments will be the players

--- game.js
+++ game.js
@@ -1,15 +1,43 @@
 // getComputerChoice();
-
+//section1//
 let randomNum =  Math.floor(Math.random() * 3);
 
 function getComputerChoice (role) {
     if (randomNum == 0){
-        return "Rock"
+        return "rock"
     } else if (randomNum == 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
+//section2
 let computer = getComputerChoice;
+
+let playerSelect = prompt("choose your role");
+
+function firstRound (bot, player){
+    if (bot == "rock" && player == "rock"){
+        return "it's a tie";
+    }else if(bot == "rock" && player == "paper"){
+        return "You win, Rock beat Paper"
+    }else if(bot == "rock" && player == "scissors")
+        return "You lose Rock beat Scissors"
+
+    if (bot == "paper" && player == "paper"){
+        return "it's a tie";
+    }else if(bot == "paper" && player == "rock"){
+        return "You lose, Paper beat Rock"
+    }else if(bot == "paper" && player == "scissors")
+        return "You win Scissors beat Paper"
+
+    if (bot == "scissors" && player == "scissors"){
+        return "it's a tie";
+    }else if(bot == "scissors" && player == "paper"){
+        return "You lose, Scissors beat Paper"
+    }else if(bot == "scissors" && player == "rock")
+        return "You win Rock beat Scissors"
+}
+
+let roundOne = firstRound;
